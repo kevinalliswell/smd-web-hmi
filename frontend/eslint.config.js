d3b199208bd0db099994076e88e03831fcb6e68c
Kevin <kevin@KevindeMac-mini.local>
2026-08-30 23:51:30 +0800

    chore(frontend): add eslint and lock dependencies

---
+++ frontend/eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+
+export default [
+  {
+    ignores: ['dist/**', 'coverage/**'],
+  },
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.{js,vue}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+]
